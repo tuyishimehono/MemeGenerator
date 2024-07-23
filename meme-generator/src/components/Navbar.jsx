2474063b2271ef6fa541c0c83baee870f4cb68db
tuyishimehono <tuhonori1@gmail.com>
2024-07-23 11:49:51 +0200

    chore: Add project setup files

---
+++ meme-generator/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import MemeLogo from '/images/meme-logo.svg'
+
+export default function Navbar() {
+    return (
+        <nav className='bg-gradient-to-r from-[#672280] to-[#A626D3] flex place-items-center justify-between flex-wrap px-10 py-6'>
+            <div className='flex gap-3'>
+                <img src={MemeLogo} alt="meme logo" />
+                <h2 className='text-white text-4xl font-bold'>Meme Generator</h2>
+            </div>
+            <div>
+                <h4 className='text-white text-lg'>React Course - Project 3</h4>
+            </div>
+        </nav>
+    )
+}
