2120e661214bb23df1fb94faec70e84c890d409f
tuyishimehono <tuhonori1@gmail.com>
2024-07-23 23:24:00 +0200

    chore: update Meme.jsx

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react"
+import memesData from '../../memesData'
 export default function Meme(){
+    const [memeImage, setMemeImage] = useState("")
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        console.log(memeImage)
+    }
     return (
-
         <main>
             <form className="px-10 pt-7">
                 <div className="grid grid-cols-2 gap-8">
                     <div className="flex flex-col gap-2">
-                        <label className="text-xl">Top text</label>
-                        <input type="text" placeholder="Shut up" className="border-2 rounded-md px-3 h-10"/>
+                        <label className="text-xl flex flex-col gap-2">Top text
+                            <input type="text" placeholder="Shut up" className="border-2 rounded-md px-3 h-10"/>
+                        </label>
                     </div>
                     <div className="flex flex-col gap-2">
-                        <label className="text-xl">Bottom text</label>
-                        <input type="text" placeholder="and take my money" className="border-2 rounded-md px-3 h-10"/>
+                        <label className="text-xl flex flex-col gap-2">Bottom text
+                            <input type="text" placeholder="and take my money" className="border-2 rounded-md px-3 h-10"/>
+                        </label>
                     </div>
                 </div>
                 <div className="pt-8">
-                    <button className="bg-gradient-to-r from-fuchsia-900 to-fuchsia-600 w-full py-3 rounded-md text-white text-3xl">Get a new meme image  🖼</button>
+                    <button onClick={getMemeImage} className="bg-gradient-to-r from-fuchsia-900 to-fuchsia-600 w-full py-3 rounded-md text-white text-3xl">Get a new meme image  🖼</button>
                 </div>
             </form>
+            <img src={memeImage} alt="meme image" className="max-w-[100%]"/>
         </main>
     )
 }
