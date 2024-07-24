b1bdc3dbf3e5051644735fb2b5dd0669a7d7706a
tuyishimehono <tuhonori1@gmail.com>
2024-07-24 20:09:01 +0200

    adjust Meme.jsx

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react"
 import memesData from '../../memesData'
 export default function Meme(){
-    const [memeImage, setMemeImage] = useState("")
-    function getMemeImage() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText:"",
+        memeImage : "https://i.imgflip.com/3lmzyx.jpg"
+    })
+    
+    function getMeme(e) {
+        e.preventDefault()
+
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
-        console.log(memeImage)
+        setMeme(prevMeme => ({...prevMeme, memeImage: memesArray[randomNumber].url}))
     }
     return (
         <main>
             <form className="px-10 pt-7">
-                <div className="grid grid-cols-2 gap-8">
+                <div className="grid grid-cols-2 gap-8 pb-8">
                     <div className="flex flex-col gap-2">
                         <label className="text-xl flex flex-col gap-2">Top text
                             <input type="text" placeholder="Shut up" className="border-2 rounded-md px-3 h-10"/>
@@ -23,11 +29,9 @@ export default function Meme(){
                         </label>
                     </div>
                 </div>
-                <div className="pt-8">
-                    <button onClick={getMemeImage} className="bg-gradient-to-r from-fuchsia-900 to-fuchsia-600 w-full py-3 rounded-md text-white text-3xl">Get a new meme image  🖼</button>
-                </div>
+                <button onClick={getMeme} className="bg-gradient-to-r from-fuchsia-900 to-fuchsia-600 w-full py-3 rounded-md text-white text-3xl">Get a new meme image  🖼</button>
             </form>
-            <img src={memeImage} alt="meme image" className="max-w-[100%]"/>
+            <img src={meme.memeImage} alt="meme image" className="max-w-[100%] pt-8 pb-20 flex place-content-centercontent-center"/>
         </main>
     )
 }
